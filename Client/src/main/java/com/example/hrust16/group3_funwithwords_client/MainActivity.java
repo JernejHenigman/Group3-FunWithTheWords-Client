9d1ce3c2bc3ffae77ae2bbf131b900fa6641686c
JernejHenigman <jernej.henigman1@gmail.com>
2015-10-11 16:26:43 +0200

    First Commit

---
+++ Client/src/main/java/com/example/hrust16/group3_funwithwords_client/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hrust16.group3_funwithwords_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
